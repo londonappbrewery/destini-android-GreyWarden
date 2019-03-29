ca65e967add57c92cbae808121d617007f3683b0
GreyWarden <raulpavonfp@gmail.com>
2019-03-29 22:12:05 +0100

    Finished

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -35,15 +35,11 @@ public class MainActivity extends AppCompatActivity {
         mTopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mStoryIndex == 1) {
+                if(mStoryIndex == 1 || mStoryIndex == 2) {
                     mTextView.setText(R.string.T3_Story);
                     mTopButton.setText(R.string.T3_Ans1);
                     mBottomButton.setText(R.string.T3_Ans2);
                     mStoryIndex = 3;
-                } else if (mStoryIndex == 2) {
-                    mTextView.setText(R.string.T3_Story);
-                    mTopButton.setText(R.string.T3_Ans1);
-                    mBottomButton.setText(R.string.T3_Ans2);
                 } else if (mStoryIndex == 3) {
                     mTextView.setText(R.string.T6_End);
                     mTopButton.setVisibility(View.GONE);
